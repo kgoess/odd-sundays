95205867b8342a93458a6da01cb125282ee22a2d
Kevin M. Goess <kevin@goess.org>
2024-08-15 14:39:40 -0700

    Define the perl_sitelib macros
    See notes in comments. Now the rpm builds and installs.

--- script/build-rpm.sh
+++ script/build-rpm.sh
@@ -7,7 +7,17 @@ make
 make test
 make dist
 cp OddSundays-*tar.gz ~/rpmbuild/SOURCES/
-rpmbuild -ba spec/perl-OddSundays.spec 
+
+# These macros used to be (centos-7) defined in /usr/lib/rpm/macros.perl (from
+# the rpm-build rpm) but in alma-9 the file (from the perl-macros rpm) does not
+# contain them.
+# They're produced by cpan2rpm https://github.com/SBECK-github/App-CPANtoRPM/
+# so we need to have them defined.
+rpmbuild \
+    --define 'perl_sitelib    %(eval "`perl -V:installsitelib`"; echo $installsitelib)'   \
+    --define 'perl_sitearch   %(eval "`perl -V:installsitearch`"; echo $installsitearch)' \
+    -ba spec/perl-OddSundays.spec
+
 
 echo "*********************"
 echo "here's your rpm!"
